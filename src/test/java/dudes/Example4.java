d55b2db748c9642b0c50b41d34ab5694da8c9902
cunger <andrea.christina.unger@gmail.com>
2016-06-27 16:12:07 +0200

    update when/where example

--- src/test/java/dudes/Example4.java
+++ src/test/java/dudes/Example4.java
@@ -17,18 +17,26 @@ public class Example4 {
         RDFDUDES someProperty = new RDFDUDES(RDFDUDES.Type.PROPERTY, "1", "2");
         
         RDFDUDES who   = expressions.wh("rdf:type","dbo:Person");
-        // RDFDUDES when  = expressions.wh("rdf:type","xsd:DateTime");
-        // RDFDUDES where = expressions.wh("rdf:type","dbo:Location");
+        RDFDUDES when  = expressions.wh("rdf:type","xsd:DateTime");
+        RDFDUDES where = expressions.wh("rdf:type","dbo:Location");
         
-        someProperty.instantiateProperty("cross");
-        someIndividual1.instantiateIndividual("Nile");
-        someIndividual2.instantiateIndividual("Brooklyn_Bridge");
+        someProperty.instantiateProperty("birth");
+        someIndividual1.instantiateIndividual("Elvis");
+        someIndividual2.instantiateIndividual("USA");
         
-        RDFDUDES d1 = someProperty.merge(someIndividual1,"2");
+        RDFDUDES d1 = someProperty.merge(someIndividual2,"2");
+        RDFDUDES d2 = someProperty.merge(someIndividual1,"1");
         
-        RDFDUDES d2 = d1.merge(who,"1"); 
-        d2.postprocess();
-        System.out.println("Who crosses Nile? " + d2.convertToSPARQL());
+        RDFDUDES d3 = d1.merge(who,"1"); 
+        d3.postprocess();
+        System.out.println("Who was born in the US?\n" + d3.convertToSPARQL());
 
+        RDFDUDES d4 = d2.merge(where,"2"); 
+        d4.postprocess();
+        System.out.println("Where was Elvis born?\n" + d4.convertToSPARQL());
+        
+        RDFDUDES d5 = d2.merge(when,"2"); 
+        d5.postprocess();
+        System.out.println("When was Elvis born?\n" + d5.convertToSPARQL());
     }
 }
